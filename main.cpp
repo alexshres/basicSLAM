ae53cdfdfd94f92915e05fb4453a0bc8463a52aa
alexshres <alexshrestha1996@gmail.com>
2025-06-24 18:42:31 -0700

    testing multi-channel

--- main.cpp
+++ main.cpp
@@ -5,26 +5,44 @@
 
 int main() {
 
-    std::vector<float> vec{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0};
-    int rows = 3;
-    int channels = 0;
+    std::vector<float> square_vec{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0};
+    std::vector<float> cube_vec{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0};
 
-    cv::Mat m{vec, false};
+    int sq_rows = 3;
+    int sq_channels = 0;
 
-    m = m.reshape(channels, rows);
+    int cube_rows = 2;
+    int cube_channels = 2;
 
-    std::cout << "Number of rows: " << m.rows 
-              << "\nNumber of columns: " << m.cols
-              << "\nNumber of channels: " << m.channels() 
+    cv::Mat sq{square_vec, false};
+    cv::Mat cube{cube_vec, false};
+
+    sq = sq.reshape(sq_channels, sq_rows);
+	cube = cube.reshape(cube_channels, cube_rows);
+
+    std::cout << "Number of rows: " << sq.rows 
+              << "\nNumber of columns: " << sq.cols
+              << "\nNumber of channels: " << sq.channels() 
               << std::endl;
 
+    std::cout << "Original Matrix:\n" << sq << std::endl;
 
-    std::cout << "Original Matrix:\n" << m << std::endl;
+    Image img_sq(sq);
 
-    Image img(m);
+    std::cout << "Image class square:\n";
+    std::cout << img_sq << std::endl;
 
-    std::cout << "Image class:\n";
-    std::cout << img << std::endl;
+    std::cout << "Number of rows: " << cube.rows 
+              << "\nNumber of columns: " << cube.cols
+              << "\nNumber of channels: " << cube.channels() 
+              << std::endl;
+
+    std::cout << "Original Matrix:\n" << cube << std::endl;
+
+    Image img_cube(cube);
+
+    std::cout << "Image class cube:\n";
+    std::cout << img_cube << std::endl;
 
     return 0;
 }
